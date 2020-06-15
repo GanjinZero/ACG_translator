26df2c25ca5b43724b7501ce4d077305ea7dfb2d
GanjinZero <mike29@126.com>
2020-06-15 11:28:15 +0800

    Add bpe and train/test split

--- bpe_util.py
+++ bpe_util.py
@@ -1,27 +1,69 @@
 import os
-from resource.load_zh_jp_transfer import load_transfer
+import json
+from collections import OrderedDict
 
 
-zh2jp, jp2zh = load_transfer()
+def learn_bpe(num_operations, vocabulary_threshold):
+    print("Learn BPE")
+    if os.path.exists(f"./bpe_model/zh_ja_{num_operations}_bpe.model"):
+        print(f"Found ./bpe_model/zh_ja_{num_operations}_bpe.model")
+    else:
+        os.system(f"subword-nmt learn-bpe -v -s {num_operations} < ./text/bpe_train.txt > ./bpe_model/zh_ja_{num_operations}_bpe.model")
+    
+    print("Apply BPE")
+    if vocabulary_threshold is None:
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model < ./text/zh_segment.txt > ./text/zh.bpe")
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model < ./text/ja_segment.txt > ./text/ja.bpe")
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model < ./text/zh_segment_test.txt > ./text/zh_test.bpe")
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model < ./text/ja_segment_test.txt > ./text/ja_test.bpe")
+    else:
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model --vocabulary-threshold {vocabulary_threshold} < ./text/zh_segment.txt > ./text/zh.bpe")
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model --vocabulary-threshold {vocabulary_threshold} < ./text/ja_segment.txt > ./text/ja.bpe")
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model --vocabulary-threshold {vocabulary_threshold} < ./text/zh_segment_test.txt > ./text/zh_test.bpe")
+        os.system(f"subword-nmt apply-bpe -c ./bpe_model/zh_ja_{num_operations}_bpe.model --vocabulary-threshold {vocabulary_threshold} < ./text/ja_segment_test.txt > ./text/ja_test.bpe")
 
-def convert_ja2zh(line):
-    opt_line = []
-    for ch in line:
-        if ch in jp2zh:
-            opt_line.append(jp2zh[ch])
-        else:
-            opt_line.append(ch)
-    return "".join(opt_line)
+    print("Build dictionary")
+    os.system(f"python build_dictionary.py ./text/zh.bpe ./text/ja.bpe")
 
-def main():
+    print("Build zh_ja dictionary")
+    zh_bpe_json = json.load(open('./text/zh.bpe.json', 'r', encoding="utf-8"))
+    ja_bpe_json = json.load(open('./text/ja.bpe.json', 'r', encoding="utf-8"))
+    zh_dict = [word for word, index in zh_bpe_json.items()]
+    ja_dict = [word for word, index in ja_bpe_json.items()]
+    zh_ja_dict = set(zh_dict + ja_dict)
+
+    zh_ja_dict = zh_ja_dict - set(['<EOS>', '<GO>', '<UNK>'])
+
+    worddict = OrderedDict()
+    worddict['<EOS>'] = 0
+    worddict['<GO>'] = 1
+    worddict['<UNK>'] = 2
+    # FIXME We shouldn't assume <EOS>, <GO>, and <UNK> aren't BPE subwords.
+    for ii, ww in enumerate(zh_ja_dict):
+        worddict[ww] = ii + 3
+
+    # Save word2id, id2word as json
+    word2id = {word: index for index, word in enumerate(worddict)}
+    id2word = {index: word for index, word in enumerate(worddict)}    
+    with open('./text/word2id.json', 'w', encoding='utf-8') as f:
+        json.dump(word2id, f, indent=2, ensure_ascii=False)
+    with open('./text/id2word.json', 'w', encoding='utf-8') as f:
+        json.dump(id2word, f, indent=2, ensure_ascii=False)
+
+    return
+
+def main(num_operations=50000, vocabulary_threshold=None):
     with open("./text/zh_segment.txt", "r", encoding="utf-8") as f:
         zh_lines = f.readlines()
     with open("./text/ja_segment.txt", "r", encoding="utf-8") as f:
         ja_lines = f.readlines()
-    ja_lines_new = [convert_ja2zh(line) for line in ja_lines]
     with open("./text/bpe_train.txt", "w", encoding="utf-8") as f:
         for line in zh_lines:
             f.write(line.strip() + "\n")
-        for line in ja_lines_new:
+        for line in ja_lines:
             f.write(line.strip() + "\n")
-    os.system("subword-nmt learn-bpe -s 40000 < ./text/bpe_train.txt > ./bpe_model/zh_ja.model")
+    
+    learn_bpe(num_operations, vocabulary_threshold)
+    
+if __name__ == "__main__":
+    main()
--- tokenize_util.py
+++ tokenize_util.py
@@ -3,6 +3,20 @@ import jieba
 import MeCab
 from tqdm import tqdm
 from resource.langconv import Converter
+from random import sample
+from resource.load_zh_jp_transfer import load_transfer
+
+
+zh2jp, jp2zh = load_transfer()
+
+def convert_ja2zh(line):
+    opt_line = []
+    for ch in line:
+        if ch in jp2zh:
+            opt_line.append(jp2zh[ch])
+        else:
+            opt_line.append(ch)
+    return "".join(opt_line)
 
 
 j_t = janome_tokenizer()
@@ -29,19 +43,37 @@ def main(zh_method="jieba", ja_method="janome"):
     with open("./text/zh.txt", "r", encoding="utf-8") as f:
         zh_lines = f.readlines()
         seg_zh_lines = [" ".join(segment_jieba(line)) for line in tqdm(zh_lines)]
-    with open("./text/zh_segment.txt", "w", encoding="utf-8") as f:
-        for line in seg_zh_lines:
-            f.write(line.strip() + "\n")
 
     with open("./text/ja.txt", "r", encoding="utf-8") as f:
         ja_lines = f.readlines()
+        """
+        Mecab is at least 30x quicker than janome.
+        """
         if ja_method == "janome":
             seg_ja_lines = [" ".join(segment_janome(line)) for line in tqdm(ja_lines)]
-        if ja_method == "janome":
+        if ja_method == "mecab":
             seg_ja_lines = [" ".join(segment_mecab(line)) for line in tqdm(ja_lines)]
+
+    ja_lines_new = [convert_ja2zh(line) for line in seg_ja_lines]
+
+    total_count = len(seg_zh_lines)
+    test_index = sample(range(total_count), 1000)
+
+    with open("./text/zh_segment.txt", "w", encoding="utf-8") as f:
+        with open("./text/zh_segment_test.txt", "w", encoding="utf-8") as f_test:
+            for index, line in enumerate(seg_zh_lines):
+                if not index in test_index:
+                    f.write(line.strip() + "\n")
+                else:
+                    f_test.write(line.strip() + "\n")
+
     with open("./text/ja_segment.txt", "w", encoding="utf-8") as f:
-        for line in seg_ja_lines:
-            f.write(line.strip() + "\n")
+        with open("./text/ja_segment_test.txt", "w", encoding="utf-8") as f_test:
+            for index, line in enumerate(ja_lines_new):
+                if not index in test_index:
+                    f.write(line.strip() + "\n")
+                else:
+                    f_test.write(line.strip() + "\n")
 
 if __name__ == "__main__":
-    main(zh_method="jieba", ja_method="janome")
+    main(zh_method="jieba", ja_method="mecab")
